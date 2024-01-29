7d3755ab91c0492b731a8b84b73c00399f5f0730
jwariara1 <jwariara1@gmail.com>
2024-01-30 00:03:42 +0300

    Our first group project

---
+++ README.md
@@ -0,0 +1,1 @@
+This is our firt group project 
---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <unistd.h>
+#include <limits.h>
+#include <stdarg.h>
+#include <stdlib.h>
+int _printf(const char *format, ...);
+#endif
+
